b42e9a59d7e7e48e959078769e4f63d75b3ca4f5
devgony <devgony@gmail.com>
2022-10-09 22:28:44 +0900

    test: modify a case for Input-parseCars to parseNames

--- src/test/java/racingcar/view/InputTest.java
+++ src/test/java/racingcar/view/InputTest.java
@@ -1,25 +1,20 @@
 package racingcar.view;
 
 import org.junit.jupiter.api.Test;
-import racingcar.model.Car;
-import racingcar.model.Cars;
-
-import java.util.Arrays;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 
 public class InputTest {
     @Test
-    void parseCars() {
-        Cars actual = Input.parseCars("a,b,c");
-        Cars expected = new Cars(Arrays.asList(new Car("a"), new Car("b"), new Car("c")));
+    void parseNames() {
+        String[] actual = Input.parseNames("a,b,c");
+        String[] expected = {"a", "b", "c"};
         assertThat(actual).isEqualTo(expected);
     }
 
-    @Test
-    void IllegalArgumentInputThrowException() {
-        assertThatThrownBy(() -> Input.parseCars("123, 123456")).isInstanceOf(IllegalArgumentException.class);
-    }
+//    @Test
+//    void IllegalArgumentInputThrowException() {
+//        assertThatThrownBy(() -> Input.parseNames("123, 123456")).isInstanceOf(IllegalArgumentException.class);
+//    }
 }
