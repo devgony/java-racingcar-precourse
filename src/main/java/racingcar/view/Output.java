33b0a8a5c2abe747e4e4e3977130a64bb812687c
devgony <devgony@gmail.com>
2022-10-09 22:29:15 +0900

    feat: implement Output.printWinners

--- src/main/java/racingcar/view/Output.java
+++ src/main/java/racingcar/view/Output.java
@@ -1,8 +1,15 @@
 package racingcar.view;
 
+import racingcar.model.Cars;
+
 public class Output {
 
     public static void println(String payload) {
         System.out.println(payload);
+    }
+
+    public static void printWinners(Cars winners) {
+        String names = winners.names();
+        println("최종 우승자: " + names);
     }
 }
